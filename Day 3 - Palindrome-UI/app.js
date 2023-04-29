3b96f9357feade3d811b4fc01136f0baa40a07ea
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-04-29 00:03:52 -0400

    removing case sensitivity from algorithm

--- Day 3 - Palindrome-UI/app.js
+++ Day 3 - Palindrome-UI/app.js
@@ -19,11 +19,12 @@ form.addEventListener("submit", function (event) {
 });
 
 function isPalindrome(word) {
+  word = word.toUpperCase(); //So that don't matter if it's lower case or uppercase the algo will work
   const wordLength = word.length;
   debugger; //use debugger to find the error in the algorithm
 
   /*
-    if character at [i] of words is not the same as character at [word.length -i-1],
+    if character at [i] of words is not the same as character at [word.length - i - 1],
      basically the first part word[i] will it iterate each letter from left to right, the other part word[wordLength - i - 1] from right to left
      , if they don't macth then is not a palindrome hence return false and set not-palindrome class, else return true and set palindrome class
      */
